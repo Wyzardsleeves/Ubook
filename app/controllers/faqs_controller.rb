28353f3705a7912899b4f0cda7db46acfb117f8f
Justin Lattimore <jlattimoreweb@gmail.com>
2019-04-16 01:42:31 -0400

    completed the FAQ page

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -5,8 +5,36 @@ class FaqsController < ApplicationController
   end
 
   def new
+    @faq = Faq.new
+  end
+
+  def create
+    @faq = Faq.new
+    @faq.question = params[:faq][:question]
+    @faq.answer = params[:faq][:answer]
+
+    if @faq.save
+      flash[:notice] = "FAQ was successfully saved!"
+      redirect_to faqs_path
+    else
+      flash[:alert] = "Error saving FAQ. Unsuccessful >.>"
+      render faqs_new_path
+    end
   end
 
   def show
+  end
+
+  def destroy
+    @faq = Faq.find(params[:id])
+    @faq.destroy
+    
+    if @faq.delete
+      flash[:notice] = "FAQ was successfully deleted!"
+      redirect_to faqs_path
+    else
+      flash[:alert] = "Error deleting this Faq. Unsuccessful >.>"
+      redirect_to faqs_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'faqs/new'
-  get 'faqs/index'
-  get 'faqs/show'
+
+  resources :faqs
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "application#index"
